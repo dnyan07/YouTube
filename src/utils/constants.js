39806492511a1ec8b74f0f5d2e0fb315d7e261e1
Dnyaneshwar Koli <dnyankoli07@gmail.com>
2023-08-17 16:32:12 +0530

    YoutubeSearch_Functinality Implimented

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,8 @@
-const API_KEY = "AIzaSyB9lo4Y9qjqcU13Y3LD49KhCdMd5RAF9jY"
+const API_KEY = "AIzaSyB9lo4Y9qjqcU13Y3LD49KhCdMd5RAF9jY";
 
-export const YOUTUBE_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + API_KEY
+export const YOUTUBE_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  API_KEY;
 
-
-
+export const YOUTUBE_SEARCH_API =
+  "http://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q=";
